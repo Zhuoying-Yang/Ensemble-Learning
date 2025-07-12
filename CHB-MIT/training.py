79ca2605b5d93a988da0e8ebf5a7f23f1557d40b
Zhuoying Yang <147845309+Zhuoying-Yang@users.noreply.github.com>
2025-07-12 19:44:44 -0400

    Update training.py

--- CHB-MIT/training.py
+++ CHB-MIT/training.py
@@ -215,7 +215,7 @@ final_pred = (final_combined >= best_thresh).astype(int)
 conf_matrix = confusion_matrix(y_test_combined, final_pred)
 report_text = classification_report(y_test_combined, final_pred)
 
-with open("hybrid_classification_report_mit_trans.txt", "w") as f:
+with open("hybrid_classification_report_mit.txt", "w") as f:
     f.write("Best ensemble weights and threshold (optimized on validation set):\n")
     f.write(str(best_weights))
     f.write(f"\nThreshold: {best_thresh:.2f}\n\n")
